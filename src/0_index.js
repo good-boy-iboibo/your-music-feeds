7a2693b228d25a8b01a616b9d51f9d1797d4b799
good-boy-iboibo <iiyybbqq@gmail.com>
2022-02-14 06:29:24 +0900

    responsive minWidth 512

--- src/0_index.js
+++ src/0_index.js
@@ -18,7 +18,7 @@ let isPC = true;
         isPC = false;
         document
             .getElementById("viewport")
-            .setAttribute("content", `width=${Math.max(window.innerWidth, 570)}`);
+            .setAttribute("content", `width=${Math.max(window.innerWidth, 512)}`);
     }
     if (window.innerWidth < 870) {
         isPC = false;
